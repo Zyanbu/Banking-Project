2e546e63e90c5150c3012ca3921a454f7187010d
Zyanbu <148467366+Zyanbu@users.noreply.github.com>
2025-02-20 14:40:01 -0500

    Include Body-Parser to server.js, set up routes for endpoints using Express

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 import express from "express";
+import bodyParser from "body-parser";
 import path, { dirname } from "path";
-import { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.listen(port, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running in port ${port}.`);
 });
 
 app.get("/", (req,res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get("/about", (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get("/login", (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get("/signup", (req,res) => {
+  res.sendFile(path.join(__dirname, 'public', 'signup.html'));
+});
+
+app.post("/signup", (req,res) => {
+  console.log(req.body);
+});
